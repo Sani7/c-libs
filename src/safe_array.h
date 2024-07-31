eca05f5c245d044c07767e7d828c763b1d31298a
Sander Speetjens <sander.speetjens@gmail.com>
2024-07-31 17:51:32 +0200

    Remove complex arrays
    This can be done with structs in an array_t

--- src/safe_array.h
+++ src/safe_array.h
@@ -18,11 +18,6 @@
 
 #define ARRAY_GET(arr, index, type) (*(type *)array_get(arr, index))
 #define ARRAY_2D_GET(arr, row, col, type) (*(type *)array_2d_get(arr, row, col))
-#define ARRAY_C_GET_IMAG(arr, row, type) (*(type *)array_c_get_imag(arr, row))
-#define ARRAY_C_GET_REAL(arr, row, type) (*(type *)array_c_get_real(arr, row))
-#define ARRAY_C_GET_MAG(arr, row, type) (*(type *)array_c_get_mag(arr, row))
-#define ARRAY_C_GET_PHASE(arr, row, type) (*(type *)array_c_get_phase(arr, row))
-
 
 typedef struct array_s
 {
@@ -41,22 +36,6 @@ typedef struct array_2d_s
     uint8_t data[1];
 } array_2d_t;
 
-typedef struct array_c_s
-{
-    size_t rows;
-    size_t size;
-    void* ptr;
-    uint8_t data[1];
-} array_c_s_t;
-
-typedef struct array_c_p_s
-{
-    size_t rows;
-    size_t size;
-    void* ptr;
-    uint8_t data[1];
-} array_c_p_t;
-
 // Array.c
 array_t *array_init(size_t size, size_t rows);
 size_t array_get_rows(array_t *arr);
@@ -70,17 +49,3 @@ size_t array_2d_get_cols(array_2d_t *x);
 void *array_2d_get(array_2d_t *x, size_t row, size_t col);
 int array_2d_set(array_2d_t *x, size_t row, size_t col, void *val);
 void array_2d_free(array_2d_t *x);
-
-array_c_s_t *array_c_s_init(size_t rows, size_t size);
-size_t array_c_s_get_rows(array_c_s_t *x);
-void* array_c_get_real(array_c_s_t *x, size_t row);
-void* array_c_get_imag(array_c_s_t *x, size_t row);
-int array_c_s_set(array_c_s_t *x, size_t row, void *real, void *imaginary);
-void array_c_s_free(array_c_s_t *x);
-
-array_c_p_t *array_c_p_init(size_t rows, size_t size);
-size_t array_c_p_get_rows(array_c_p_t *x);
-void *array_c_get_mag(array_c_p_t *x, size_t row);
-void *array_c_get_phase(array_c_p_t *x, size_t row);
-int array_c_p_set(array_c_p_t *x, size_t row, void* magnitude, void* phase);
-void array_c_p_free(array_c_p_t *x);
